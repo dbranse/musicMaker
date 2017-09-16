bada4d08a52114419bf465cbd3f06b36f11e4da3
GeneralSherman <markos.business@gmail.com>
2017-09-16 16:34:46 -0400

    added more comments

--- drums/simple_drummer.py
+++ drums/simple_drummer.py
@@ -9,14 +9,16 @@ clap = None
 d = {0: bass, 1: snare, 2: hihat, 3: clap}
 
 beatarr = [[0], [1, 2], [3], [1, 2]]
-bpm = 60
-l = 60
+bpm = 100
+
+#l is the number of beats
+l = 100
 
 #will loop until the number of beats is done
 for i in range(l):
-	# wil loop through the beat array at the correct index
-	for sound in beatarr[i%len(beatarr)]:
-		# we need to play asyncronously
-		d[sound].play()
-	# wait until the next beat
-	time.sleep(bpm/60)
+    # wil loop through the beat array at the correct index
+    for sound in beatarr[i%len(beatarr)]:
+        # we need to play asyncronously
+        d[sound].play()
+    # wait until the next beat
+    time.sleep(bpm/60)
