5608000df3d6ebd53ad5638320f5a9d6e02f7899
GeneralSherman <markos.business@gmail.com>
2017-09-17 01:43:28 -0400

    we got a mvp bois

--- drums/simple_drummer.py
+++ drums/simple_drummer.py
@@ -1,17 +1,32 @@
 import time
 import numpy as np
 import sounddevice as sd
+from threading import Thread
 from pydub import AudioSegment
+from pydub.playback import play
 #sampling rate
 fs = 44100
 
-AudioSegment.converter = '/Users/Marko/Downloads/Lion_Mountain_Lion_Mavericks_Yosemite_El-Captain_15.05.2017/ffmpeg'
+# AudioSegment.converter = '/Users/Marko/Downloads/Lion_Mountain_Lion_Mavericks_Yosemite_El-Captain_15.05.2017/ffmpeg'
 
 def transform_file(flpath):
 
-    def detect_leading_silence(sound, silence_threshold=-40.0, chunk_size=10):
+    def detect_leading_silence(sound, silence_threshold=-30.0, chunk_size=10):
         trim_ms = 0 # ms
         while sound[trim_ms:trim_ms+chunk_size].dBFS < silence_threshold:
+            trim_ms += chunk_size
+
+        return trim_ms
+
+    def detect_max_vol(sound, chunk_size=10):
+        trim_ms = 0 # ms
+        m = -10000
+        m_ind = 0
+        while trim_ms+chunk_size < len(sound):
+            db = sound[trim_ms:trim_ms+chunk_size].dBFS
+            if db > m:
+                m = db
+                m_ind = trim_ms
             trim_ms += chunk_size
 
         return trim_ms
@@ -21,17 +36,17 @@ def transform_file(flpath):
     start_trim = detect_leading_silence(sound)
     end_trim = detect_leading_silence(sound.reverse())
 
-    trimmed_sound = sound#[start_trim:-end_trim]
+    trimmed_sound = sound[start_trim:-end_trim]
 
-    return trimmed_sound.get_array_of_samples()
+    return trimmed_sound, detect_max_vol(trimmed_sound)
 
 
 
 # these will eventually be music files
-bass = np.array(transform_file('bass.wav')).astype(np.int32)
-snare = np.array(transform_file('snare.wav')).astype(np.int32)
-hihat = np.array(transform_file('hihat.wav')).astype(np.int32)
-clap = np.array(transform_file('clap.wav')).astype(np.int32)
+bass = transform_file('bass.wav')
+snare = transform_file('snare.wav')
+hihat = transform_file('hihat.wav')
+clap = transform_file('clap.wav')
 
 d = {0: bass, 1: snare, 2: hihat, 3: clap}
 
@@ -39,16 +54,26 @@ beatarr = [[0], [1, 2], [3], [1, 2]] #each element contains things that should p
 simplebeats = [[[0,2],[1,2],[0,2],[1,2]], [[0],[2],[3],[2]], [[0,1,2],[2,3],[1,2],[2,3]], [[0,2],[1,3],[1,2],[1,3]], [[0,2],[1,2],[0,2],[1,2]]]
 moderatebeats = [[[0,2],[2],[1,2],[2],[0,2],[2],[1,2,3],[2,3]], [[0], [2],[3],[2],[0], [2,1],[3,1],[2,1]], [[0,2], [2],[3,2],[0,2],[2], [2],[3,2],[2]], [[0,2], [2],[2],[1,3,2],[2], [2],[1,3,2],[2]], [[0,2], [0,2],[3,2],[1,2],[2], [0,2],[3,2],[1,2]]]
 advancedbeats = [[[0,2],[2],[0,2],[2],[3,2],[2],[0,3],[2],[1,2],[0,2],[0,2],[2],[3,2],[2,1],[0,3,1],[2,1]], [[0,2],[1,2],[3,2],[1,2],[0,2],[1,2],[3,2],[1,2],[0,2],[1,2],[3,2],[1,2],[0,2],[1,2],[1,2],[1,2]], [[0,2],[2],[2],[2],[0],[],[1,2],[0],[2],[3],[3,2],[],[0,2],[2],[1],[2]], [[0,2], [2],[2],[1,3,2],[2], [2],[1,3,2],[2]], [[0,2],[2],[3,2],[0,2],[0,2],[0,2],[2],[3,2],[0,2],[1,2],[3,2],[1,2],[0,2],[1],[1,2],[1,2]]]
-bpm = 100
+bpm = 120
 
+beatarr = moderatebeats[3]
+bpm*=2
 #l is the number of beats
 l = 100
 
 #will loop until the number of beats is done
 for i in range(l):
     # wil loop through the beat array at the correct index
+    beat_len = 60/float(bpm)*1000
+    s = AudioSegment.silent(beat_len)
+    smax = None
+    
     for sound in beatarr[i%len(beatarr)]:
         # we need to play asyncronously
-        sd.play(d[sound], fs, blocking = False)
+        s2, s2max = d[sound]
+        s = s.overlay(s2, position = beat_len//2 - s2max)
+
+    Thread(target = play, args = (s,)).start()
+
     # wait until the next beat
-    time.sleep(bpm/60)
+    time.sleep(60/float(bpm))
