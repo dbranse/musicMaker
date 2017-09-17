1a27e42328e65eab17bace1f91abdd53698ce93d
vgneus <gadh@rajits-MacBook-Pro.local>
2017-09-17 00:09:13 -0400

    website updates

--- website/musicmaker.php
+++ website/musicmaker.php
@@ -3,17 +3,14 @@
 
 <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 
-    <!-- site-wide CSS-->
-<link rel="stylesheet" type="text/css" href="musicmaker.css">
+ 
     
     <!-- import jquery-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     
     <script>
 
-        window.addEventListener('load', function() {
-        // Incrementally call getTweets every 5 seconds
-            click();
+        window.addEventListener('load', function() {click();
         }, false);
     
         function gotSound() { //asking if the server got the sound
@@ -29,8 +26,11 @@
         }
     
         function main(){
-                
-                
+                if (click()){
+                    
+                    setTimeout(myFunction, 3000);
+                }
+            
             
             
             
@@ -39,18 +39,19 @@
     
     
     </script>
-    
-    
-    
-    </head>
-
-<!-- fonts-->
+    <!-- fonts-->
 <link href='https://fonts.googleapis.com/css?family=Arvo|Bree+Serif|Varela+Round|Signika:400,600,300' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="MyFontsWebfontsKit.css">
 
 <!-- glyphicons-->
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+       <!-- site-wide CSS-->
+<link rel="stylesheet" type="text/css" href="musicmaker.css">
+    
+    </head>
+
+
 
 <body>
 
@@ -62,11 +63,16 @@
     <div class="text-panel">
     To begin, hold the controller 3 inches (7.5 cm) from your face and press the go button<span class="text-panel-green"></span>.
         <span class="glyphicon glyphicon-hand-right"></span>
+        
+        
     </div>
     
     
-    
-    
+    <div class=".circle-panel">
+        <img src="http://varungadh.com/hackmit.png" href="" style="display: block;
+    margin: auto; margin-top: -270px; opacity: .9;
+    width: 40%;">
+    </div>
     
     
 </body>
