301b43221075537bcb87da28ee09710a49e97c83
David Branse <David@Davids-MacBook-Pro-3.local>
2017-09-17 10:43:38 -0400

    temporary backend commenting out

--- backend.py
+++ backend.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-import pyaudio
-import wave
-from array import array
+#import pyaudio
+#import wave
+#from array import array
 import os
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ class Click(Resource):
 
     def post(self):
         wasClicked = self.isClicked
-        isClicked = False
+        # isClicked = False
         return {'isClicked': wasClicked, 'status': 200}
 
     def get(self):
@@ -27,48 +27,48 @@ class Sounds(Resource):
     hihat = None
     clap = None
 
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 2
-    RATE = 44100
-    CHUNK = 1024
-    RECORD_SECONDS = 15
+    #FORMAT = pyaudio.paInt16
+    #CHANNELS = 2
+    #RATE = 44100
+    #CHUNK = 1024
+    #RECORD_SECONDS = 15
 
     def post(self, name):
-        audio = pyaudio.PyAudio()  # instantiate the pyaudio
-
-        # recording prerequisites
-        stream = audio.open(format=self.FORMAT, channels=self.CHANNELS,
-                            rate=self.RATE,
-                            input=True,
-                            frames_per_buffer=self.CHUNK)
-
-        # starting recording
-        frames = []
-
-        for i in range(0, int(self.RATE / self.CHUNK * self.RECORD_SECONDS)):
-            data = stream.read(self.CHUNK)
-            data_chunk = array('h', data)
-            vol = max(data_chunk)
-            if (vol >= 500):
-                print("something said")
-                frames.append(data)
-            else:
-                print("nothing")
-            print("\n")
-
-        # end of recording
-        stream.stop_stream()
-        stream.close()
-        audio.terminate()
-        # writing to file
-        FILE_NAME = name + ".wav"
-        wavfile = wave.open(FILE_NAME, 'wb')
-        wavfile.setnchannels(self.CHANNELS)
-        wavfile.setsampwidth(audio.get_sample_size(self.FORMAT))
-        wavfile.setframerate(self.RATE)
-        wavfile.writeframes(b''.join(frames))  # append frames recorded to file
-        wavfile.close()
-
+        # audio = pyaudio.PyAudio()  # instantiate the pyaudio
+        #
+        # # recording prerequisites
+        # stream = audio.open(format=self.FORMAT, channels=self.CHANNELS,
+        #                     rate=self.RATE,
+        #                     input=True,
+        #                     frames_per_buffer=self.CHUNK)
+        #
+        # # starting recording
+        # frames = []
+        #
+        # for i in range(0, int(self.RATE / self.CHUNK * self.RECORD_SECONDS)):
+        #     data = stream.read(self.CHUNK)
+        #     data_chunk = array('h', data)
+        #     vol = max(data_chunk)
+        #     if (vol >= 500):
+        #         print("something said")
+        #         frames.append(data)
+        #     else:
+        #         print("nothing")
+        #     print("\n")
+        #
+        # # end of recording
+        # stream.stop_stream()
+        # stream.close()
+        # audio.terminate()
+        # # writing to file
+        # FILE_NAME = name + ".wav"
+        # wavfile = wave.open(FILE_NAME, 'wb')
+        # wavfile.setnchannels(self.CHANNELS)
+        # wavfile.setsampwidth(audio.get_sample_size(self.FORMAT))
+        # wavfile.setframerate(self.RATE)
+        # wavfile.writeframes(b''.join(frames))  # append frames recorded to file
+        # wavfile.close()
+        wavfile = None
         if (name == 'bass'):
             bass = wavfile
         if (name == 'snare'):
